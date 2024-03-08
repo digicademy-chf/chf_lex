adb66eb3e03fc3eeb757c9e08db7865163d9aa0e
Jonatan Jalle Steller <jonatan.steller@adwmainz.de>
2024-03-08 12:21:21 +0100

    Update header of old TCA files

--- Configuration/JavaScriptModules.php
+++ Configuration/JavaScriptModules.php
@@ -1,10 +1,13 @@
 <?php
+declare(strict_types=1);
 
 # This file is part of the extension CHF Lex for TYPO3.
 #
 # For the full copyright and license information, please read the
 # LICENSE.txt file that was distributed with this source code.
 
+
+defined('TYPO3') or die();
 
 // Map all *.js files in a specific folder to an @ module key
 return [
@@ -13,5 +16,3 @@ return [
         '@digicademy/chf_lex/' => 'EXT:chf_lex/Resources/Public/JavaScript/',
     ],
 ];
-
-?>
