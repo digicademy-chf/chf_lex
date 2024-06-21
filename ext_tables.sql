7f49db3f365e22290e5e1c55d543155281c7abc3
Jonatan Jalle Steller <jonatan.steller@adwmainz.de>
2024-06-22 01:20:31 +0200

    Add MM table fields and unify floor plan fields

--- ext_tables.sql
+++ ext_tables.sql
@@ -30,3 +30,70 @@ CREATE TABLE tx_chflex_domain_model_transcription (
 CREATE TABLE tx_chfbase_domain_model_tag (
     for varchar(255) DEFAULT '' NOT NULL
 );
+
+# Remove when forge.typo3.org/issues/98322 is fixed to auto-generate these fields
+
+CREATE TABLE tx_chflex_domain_model_any_member_ref_mm (
+	fieldname varchar(63) DEFAULT '' NOT NULL
+	tablename varchar(63) DEFAULT '' NOT NULL
+);
+
+CREATE TABLE tx_chflex_domain_model_definition_tag_definitiontype_mm (
+	fieldname varchar(63) DEFAULT '' NOT NULL
+	tablename varchar(63) DEFAULT '' NOT NULL
+);
+
+CREATE TABLE tx_chflex_domain_model_dictionary_entry_tag_label_mm (
+	fieldname varchar(63) DEFAULT '' NOT NULL
+	tablename varchar(63) DEFAULT '' NOT NULL
+);
+
+CREATE TABLE tx_chflex_domain_model_dictionary_entry_tag_partofspeech_mm (
+	fieldname varchar(63) DEFAULT '' NOT NULL
+	tablename varchar(63) DEFAULT '' NOT NULL
+);
+
+CREATE TABLE tx_chflex_domain_model_encyclopedia_entry_tag_label_mm (
+	fieldname varchar(63) DEFAULT '' NOT NULL
+	tablename varchar(63) DEFAULT '' NOT NULL
+);
+
+CREATE TABLE tx_chflex_domain_model_example_tag_label_mm (
+	fieldname varchar(63) DEFAULT '' NOT NULL
+	tablename varchar(63) DEFAULT '' NOT NULL
+);
+
+CREATE TABLE tx_chflex_domain_model_frequency_feature_geodata_mm (
+	fieldname varchar(63) DEFAULT '' NOT NULL
+	tablename varchar(63) DEFAULT '' NOT NULL
+);
+
+CREATE TABLE tx_chflex_domain_model_inflected_form_tag_inflectiontype_mm (
+	fieldname varchar(63) DEFAULT '' NOT NULL
+	tablename varchar(63) DEFAULT '' NOT NULL
+);
+
+CREATE TABLE tx_chflex_domain_model_inflected_form_tag_label_mm (
+	fieldname varchar(63) DEFAULT '' NOT NULL
+	tablename varchar(63) DEFAULT '' NOT NULL
+);
+
+CREATE TABLE tx_chflex_domain_model_member_tag_role_mm (
+	fieldname varchar(63) DEFAULT '' NOT NULL
+	tablename varchar(63) DEFAULT '' NOT NULL
+);
+
+CREATE TABLE tx_chflex_domain_model_pronunciation_tag_label_mm (
+	fieldname varchar(63) DEFAULT '' NOT NULL
+	tablename varchar(63) DEFAULT '' NOT NULL
+);
+
+CREATE TABLE tx_chflex_domain_model_sense_tag_label_mm (
+	fieldname varchar(63) DEFAULT '' NOT NULL
+	tablename varchar(63) DEFAULT '' NOT NULL
+);
+
+CREATE TABLE tx_chflex_domain_model_transcription_tag_scheme_mm (
+	fieldname varchar(63) DEFAULT '' NOT NULL
+	tablename varchar(63) DEFAULT '' NOT NULL
+);
